455841a27264a8a6e6715cec2e20d4d4391e1c9c
= <gngroenewegen@gmail.com>
2020-09-05 12:18:56 +0200

    Add custom class starter weapons

--- src/hooks/menu_hook.h
+++ src/hooks/menu_hook.h
@@ -9,7 +9,7 @@ GETTER_VAR(void*, ASMSetClassName_1_ret);
 GETTER_VAR(void*, ASMSetClassName_2_ret);
 GETTER_VAR(void*, OverwriteClassIdIncrease_ret);
 GETTER_VAR(void*, OverwriteClassIdDecrease_ret);
-
+GETTER_VAR(void*, AddCreatureStarterWeapons_ret);
 
 const char roguename_0[] = "Rogue";
 char custom_class_char[16];
@@ -60,6 +60,23 @@ extern "C" void get_character_menu_id_increase(cube::CharacterStyleWidget* widge
 
 extern "C" void get_character_menu_id_decrease(cube::CharacterStyleWidget * widget) {
 	widget->class_type = GetPreviousClassId(widget->class_type);
+}
+
+extern "C" char generate_random_creature_info(cube::Game* game) {
+	cube::Creature* player = game->GetPlayer();
+	int classType = player->entity_data.classType;
+	if (classType > 4) {
+		Class* classInstance = GetClassByGameId(classType);
+		if (classInstance->itemClass == 0) {
+			player->entity_data.classType = 1;
+		}
+		else {
+			player->entity_data.classType = classInstance->itemClass;
+		}
+	}
+	char ret = ((char (*)(cube::Game*))CWOffset(0x913B0))(game);
+	player->entity_data.classType = classType;
+	return ret;
 }
 
 void ASMSetMenuClassName() asm_naked {
@@ -154,6 +171,21 @@ void ASMMenuClassIdDecrease() asm_naked {
 	);
 }
 
+void ASMAddCreatureStarterWeapons() asm_naked {
+	asm(".intel_syntax \n"
+		PUSH_ALL
+		"mov rcx, [rdi + 0x1E8] \n"
+		PREPARE_STACK
+		"call generate_random_creature_info \n"
+		RESTORE_STACK
+		POP_ALL
+		"mov rcx, [rdi + 0x1E8] \n"
+		"mov rcx, [rcx + 0x08] \n"
+		DEREF_JMP(AddCreatureStarterWeapons_ret)
+		".att_syntax \n"
+	);
+}
+
 void AddMenuClassNames() {
 	char* base = (char*)CWBase();
 	WriteFarJMP(base + 0x2714CF, (void*)&ASMSetMenuClassName);
@@ -180,9 +212,16 @@ void OverwriteClassIdDecrease() {
 	OverwriteClassIdDecrease_ret = (base + 0x27240A);
 }
 
+void AddCreatureStarterWeapons() {
+	char* base = (char*)CWBase();
+	WriteFarJMP(base + 0x273176, (void*)&ASMAddCreatureStarterWeapons);
+	AddCreatureStarterWeapons_ret = (base + 0x27318D);
+}
+
 void InitializeMenuHook() {
 	AddMenuClassNames();
 	AddClassNames();
 	OverwriteClassIdIncrease();
 	OverwriteClassIdDecrease();
+	AddCreatureStarterWeapons();
 }
